14c9b00b6a279d6a7346e3c6159a2d0ba4c8b151
Isabella <bitetto@coispa.it>
2020-02-27 16:00:35 +0100

    check_quasiidentical_records

--- RoME/R/check_quasiidentical_records.r
+++ RoME/R/check_quasiidentical_records.r
@@ -1,137 +1,134 @@
 ###########################################################################################################################
-#   RoME: R code to perform multiple checks on MEDITS Survey data (TA, TB, TC and TE files - old and new MEDITS formats)                                   #                   
-#   Authors: I. Bitetto, M.T. Facchini, M.T. Spedicato                                                                    #
+#   RoME: R code to perform multiple checks on MEDITS Survey data (TA, TB, TC and TE files - old and new MEDITS formats)                                   #
+#   Authors: I. Bitetto, W. Zupa, M.T. Spedicato                                                                    #
 #   Coispa Tecnologia & Ricerca - Stazione sperimentale per lo Studio delle Risorse del Mare                              #
-#   If you have any comments or suggestions please contact the following e-mail address: bitetto@coispa.it                #                                                                                  
-#   March 2013                                                                                                            #
+#   If you have any comments or suggestions please contact the following e-mail address: bitetto@coispa.it, zupa@coispa.eu                #
+#   March 2020                                                                                                            #
 ###########################################################################################################################
+
 # Check quasi-identical records
 
-check_quasiidentical_records<-function(Result,Table){
-  #Result = read.csv(paste(Data,".csv",sep=""), sep=";", header=TRUE)
-#   if (Type_of_files==".csv"){
-#     #write.xls(Result,file=paste(Data,".xls", sep = ""))
-#   }
-  check_without_errors = FALSE 
-  
+if (FALSE){
+  Result = read.csv("C:/Users/Bitetto Isabella/OneDrive - Coispa Tecnologia & Ricerca S.C.A.R.L/Rome/ROME/data/TA_GSA18_1994-2018.csv", sep=";")
+  Result = read.csv("C:/Users/Bitetto Isabella/OneDrive - Coispa Tecnologia & Ricerca S.C.A.R.L/Rome/ROME/data/TB_GSA18_1994-2018.csv", sep=";")
+  Result = read.csv("C:/Users/Bitetto Isabella/OneDrive - Coispa Tecnologia & Ricerca S.C.A.R.L/Rome/ROME/data/TC_GSA18_1994-2018.csv", sep=";")
+
+  wd <- "C:/Users/Bitetto Isabella/OneDrive - Coispa Tecnologia & Ricerca S.C.A.R.L/Rome/ROME/temp"
+  suffix=paste(as.character(Sys.Date()),format(Sys.time(), "_time h%Hm%Ms%OS0"),sep="")
+
+}
+
+
+
+check_quasiidentical_records<-function(Result,wd,suffix){
+  Table=as.character(Result[1,1])
+  check_without_errors = FALSE
+
   # ------------------------------------------------------------------ TA
-  
+
   if (Table == "TA"){
     write(paste("
                 ----------- check quasi identical records - ",Result$YEAR[1]), file = Errors, append = TRUE)
-    write(paste("TA:"), file = Errors, append = TRUE)                                                                                                   
-    
-    
-    #channel <- odbcConnectExcel(paste(Data,".xls", sep = ""))
-    Matrix=sqldf("select count(*) as count, TYPE_OF_FILE, AREA, VESSEL, GEAR, RIGGING, DOORS, YEAR from Result Group by TYPE_OF_FILE, AREA, VESSEL, GEAR, RIGGING, DOORS, YEAR")
-   
-    
+    write(paste("TA:"), file = Errors, append = TRUE)
+
+     #Matrix=sqldf("select count(*) as count, TYPE_OF_FILE, AREA, VESSEL, GEAR, RIGGING, DOORS, YEAR from Result Group by TYPE_OF_FILE, AREA, VESSEL, GEAR, RIGGING, DOORS, YEAR")
+     Matrix=stats::aggregate(Result$TYPE_OF_FILE,by=list(Result$TYPE_OF_FILE, Result$AREA, Result$VESSEL, Result$GEAR, Result$RIGGING, Result$DOORS, Result$YEAR),FUN="length")
+
     if (nrow(Matrix)>1){
-      Max=max(Matrix$count)
+      Max=max(Matrix$x)
       Matrix2=Matrix[Matrix$count!=Max,]
-      
-      ResultData = sqldf("select * from Result where HAUL_NUMBER is not null")
-      
+
+      ResultData = Result[!is.na(Result$HAUL_NUMBER),]
+        #sqldf("select * from Result where HAUL_NUMBER is not null")
+
       for (i in 1:nrow(Matrix2)){
-        Err = ResultData[as.character(ResultData$TYPE_OF_FILE) == as.character(Matrix2$TYPE_OF_FILE[i]) & 
+        Err = ResultData[as.character(ResultData$TYPE_OF_FILE) == as.character(Matrix2$TYPE_OF_FILE[i]) &
                            as.character(ResultData$AREA) == as.character(Matrix2$AREA[i]) &
                            as.character(ResultData$VESSEL) == as.character(Matrix2$VESSEL[i]) &
                            as.character(ResultData$GEAR) == as.character(Matrix2$GEAR[i]) &
                            as.character(ResultData$RIGGING) == as.character(Matrix2$RIGGING[i]) &
                            as.character(ResultData$DOORS) == as.character(Matrix2$DOORS[i]) &
-                           as.character(ResultData$YEAR) == as.character(Matrix2$YEAR[i]),] 
-        
-        
+                           as.character(ResultData$YEAR) == as.character(Matrix2$YEAR[i]),]
+
+
         for( k in 1:nrow(Err)){
           write(paste("Haul ",Err$HAUL_NUMBER[k],
-                      "there is an inconsistent value in one or more of the fields that should be always identical in", 
+                      "there is an inconsistent value in one or more of the fields that should be always identical in",
                       Matrix$TYPE_OF_FILE[1]), file = Errors, append = TRUE)
         }
-      }  
-    } else { 
+      }
+    } else {
       write(paste("No error occurred"), file = Errors, append = TRUE)
       check_without_errors = TRUE
     }
-   # odbcClose(channel)
-    
-    
-  } else if (Table=="TB") { 
+
+
+  } else if (Table=="TB") {
     # ------------------------------------------------------------------ TB
-    write(paste("TB:"), file = Errors, append = TRUE)                                                                                                       
-    #Result = read.csv(paste(Data,".csv",sep=""), sep=";", header=TRUE)
-    if (Type_of_files==".csv"){
-     # write.xls(Result,file=paste(Data,".xls", sep = ""))
-    }
-    
-    #channel <- odbcConnectExcel(paste(Data,".xls", sep = ""))
-    Matrix=sqldf("select count(*) as count, TYPE_OF_FILE, AREA, VESSEL, YEAR from Result Group by TYPE_OF_FILE,  AREA, VESSEL, YEAR")
-    
-    ResultData = sqldf("select * from Result where HAUL_NUMBER is not null")
-    
+    write(paste("TB:"), file = Errors, append = TRUE)
+
+     # Matrix=sqldf("select count(*) as count, TYPE_OF_FILE, AREA, VESSEL, YEAR from Result Group by TYPE_OF_FILE,  AREA, VESSEL, YEAR")
+      Matrix=stats::aggregate(Result$TYPE_OF_FILE,by=list(Result$TYPE_OF_FILE, Result$AREA, Result$VESSEL, Result$YEAR),FUN="length")
+
+      ResultData = Result[!is.na(Result$HAUL_NUMBER),]
+
     if (nrow(Matrix)>1){
-      Max=max(Matrix$count)
+      Max=max(Matrix$x)
       Matrix2=Matrix[Matrix$count!=Max,]
       for (i in 1:nrow(Matrix2)){
-        
-        Err = ResultData[as.character(ResultData$TYPE_OF_FILE) == as.character(Matrix2$TYPE_OF_FILE[i]) & 
+
+        Err = ResultData[as.character(ResultData$TYPE_OF_FILE) == as.character(Matrix2$TYPE_OF_FILE[i]) &
                            as.character(ResultData$AREA) == as.character(Matrix2$AREA[i]) &
                            as.character(ResultData$VESSEL) == as.character(Matrix2$VESSEL[i]) &
-                           as.character(ResultData$YEAR) == as.character(Matrix2$YEAR[i]),] 
-        
+                           as.character(ResultData$YEAR) == as.character(Matrix2$YEAR[i]),]
+
         for( k in 1:nrow(Err)){
-          if (Err$YEAR[k] == Matrix2$YEAR[i]){ 
+          if (Err$YEAR[k] == Matrix2$YEAR[i]){
             write(paste("Haul ",Err$HAUL_NUMBER[k],Err$GENUS[k],Err$SPECIES[k],"there is an inconsistent value in one or more of the fields that should be always identical in", Matrix$TYPE_OF_FILE[1]), file = Errors, append = TRUE)
           }
         }
-      }  
+      }
     } else
     {
       write(paste("No error occurred"), file = Errors, append = TRUE)
       check_without_errors = TRUE
     }
-    #odbcClose(channel)
-  } else if (Table=="TC"){  
+
+  } else if (Table=="TC"){
     # ------------------------------------------------------------------ TC
-    write(paste("TC:"), file = Errors, append = TRUE)                                                                                                         
-    #Result = read.csv(paste(Data,".csv",sep=""), sep=";", header=TRUE)
-    if (Type_of_files==".csv"){
-     # write.xls(Result,file=paste(Data,".xls", sep = ""))
-    }
-    
-    #channel <- odbcConnectExcel(paste(Data,".xls", sep = ""))
-    Matrix=sqldf("select count(*) as count, TYPE_OF_FILE, AREA, VESSEL, YEAR from Result Group by TYPE_OF_FILE, AREA, VESSEL, YEAR")
-    
-    ResultData = sqldf("select * from Result where HAUL_NUMBER is not null")
-    
+    write(paste("TC:"), file = Errors, append = TRUE)
+
+ # Matrix=sqldf("select count(*) as count, TYPE_OF_FILE, AREA, VESSEL, YEAR from Result Group by TYPE_OF_FILE, AREA, VESSEL, YEAR")
+  Matrix=stats::aggregate(Result$TYPE_OF_FILE,by=list(Result$TYPE_OF_FILE, Result$AREA, Result$VESSEL, Result$YEAR),FUN="length")
+
+  ResultData = Result[!is.na(Result$HAUL_NUMBER),]
+
+
     if (nrow(Matrix)>1){
-      Max=max(Matrix$count)
+      Max=max(Matrix$x)
       Matrix2=Matrix[Matrix$count!=Max,]
       for (i in 1:nrow(Matrix2)){
-        
-        Err = ResultData[as.character(ResultData$TYPE_OF_FILE) == as.character(Matrix2$TYPE_OF_FILE[i]) & 
+
+        Err = ResultData[as.character(ResultData$TYPE_OF_FILE) == as.character(Matrix2$TYPE_OF_FILE[i]) &
                            as.character(ResultData$AREA) == as.character(Matrix2$AREA[i]) &
                            as.character(ResultData$VESSEL) == as.character(Matrix2$VESSEL[i]) &
-                           as.character(ResultData$YEAR) == as.character(Matrix2$YEAR[i]),] 
-        
+                           as.character(ResultData$YEAR) == as.character(Matrix2$YEAR[i]),]
+
         for( k in 1:nrow(Err)){
-          if (Err$YEAR[k] == Matrix2$YEAR[i]){ 
+          if (Err$YEAR[k] == Matrix2$YEAR[i]){
             write(paste("Haul",Err$HAUL_NUMBER[k],Err$GENUS[k],Err$SPECIES[k],ifelse(Err$SEX[k]=="FALSE","F", "M"),"length",Err$LENGTH_CLASS[k],"there is an inconsistent value in one or more of the fields that should be always identical in", Matrix$TYPE_OF_FILE[1]), file = Errors, append = TRUE)
           }
         }
-      }  
+      }
     } else
     {
       write(paste("No error occurred"), file = Errors, append = TRUE)
       check_without_errors = TRUE
     }
-    
-    #odbcClose(channel)
+
+
   }
-  
-#   if (Type_of_files==".csv"){
-#     unlink(paste(Data,".xls", sep = ""))
-#     }
-#   
+
 
 return(check_without_errors)
 }
