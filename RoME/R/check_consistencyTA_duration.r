cc016d1a4730e94436a17971ad623a65ef460ba0
Walter <zupa@coispa.eu>
2020-02-28 16:49:52 +0100

    TA_duration

--- RoME/R/check_consistencyTA_duration.r
+++ RoME/R/check_consistencyTA_duration.r
@@ -13,7 +13,7 @@ check_consistencyTA_duration<-function(DataTA, wd, suffix){
     library(RoME)
     wd <- tempdir()
     suffix=paste(as.character(Sys.Date()),format(Sys.time(), "_time h%Hm%Ms%OS0"),sep="")
-    DataTA = MEDITS::TA
+    DataTA = read.csv("~/GitHub/RoME/data/TA_GSA18_1994-2018.csv", sep=";") # MEDITS::TA
     # check_consistencyTA_duration(DataTA, wd, suffix)
   }
 
@@ -32,6 +32,7 @@ check_consistencyTA_duration<-function(DataTA, wd, suffix){
 
   Matrix=Matrix[Matrix$VALIDITY=="V",]
   Matrix$Start=timeDate("01-01-2001.0000", format = "%d-%m-%Y.%H%M", zone = "GMT", FinCenter = "GMT")
+  i=1
   for (i in 1:nrow(Matrix)){
     Matrix$Start[i]= timeDate(paste(paste(ifelse(str_length(Matrix$DAY[i])==1,paste("0",Matrix$DAY[i],sep=""),Matrix$DAY[i]),ifelse(str_length(Matrix$MONTH[i])==1,paste("0",Matrix$MONTH[i],sep=""),Matrix$MONTH[i]),Matrix$YEAR[i],sep="-"),ifelse(str_length(Matrix$SHOOTING_TIME[i])==3,paste("0",Matrix$SHOOTING_TIME[i],sep=""),Matrix$SHOOTING_TIME[i]),sep="."), format = "%d-%m-%Y.%H%M", zone = "GMT", FinCenter = "GMT")
   }
@@ -40,7 +41,8 @@ check_consistencyTA_duration<-function(DataTA, wd, suffix){
     Matrix$End[i]= timeDate(paste(paste(ifelse(str_length(Matrix$DAY[i])==1,paste("0",Matrix$DAY[i],sep=""),Matrix$DAY[i]),ifelse(str_length(Matrix$MONTH[i])==1,paste("0",Matrix$MONTH[i],sep=""),Matrix$MONTH[i]),Matrix$YEAR[i],sep="-"),ifelse(str_length(Matrix$HAULING_TIME[i])==3,paste("0",Matrix$HAULING_TIME[i],sep=""),Matrix$HAULING_TIME[i]),sep="."), format = "%d-%m-%Y.%H%M", zone = "GMT", FinCenter = "GMT")
   }
   Matrix$difference=Matrix$End-Matrix$Start
-  # one_hour = difftime(timeDate("01-01-2001.0200", format = "%d-%m-%Y.%H%M", zone = "GMT", FinCenter = "GMT"), timeDate("01-01-2001.0100", format = "%d-%m-%Y.%H%M", zone = "GMT", FinCenter = "GMT"),units="mins")
+
+    j=1
   for (j in nrow(Matrix))  {
     if (is.na(str_extract(Matrix$difference[j],"[0-9]{3,}?"))){
       Matrix$difference[j]=str_extract(Matrix$difference[j],"[0-9]{2,}?")  } else {
@@ -66,4 +68,3 @@ check_consistencyTA_duration<-function(DataTA, wd, suffix){
   } else { return(FALSE) }
 }
 
-################################################################################
