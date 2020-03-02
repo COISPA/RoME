b0c31be50c7f8f2dee8c3d26c42742943dfa217e
Isabella <bitetto@coispa.it>
2020-03-02 12:44:12 +0100

    corrected

--- RoME/R/check_species_TBTC.r
+++ RoME/R/check_species_TBTC.r
@@ -18,7 +18,7 @@ if (FALSE){
   load("C:/Users/Bitetto Isabella/OneDrive - Coispa Tecnologia & Ricerca S.C.A.R.L/Rome/ROME//RoME//data//DataTargetSpecies.rda")
   load("C:/Users/Bitetto Isabella/OneDrive - Coispa Tecnologia & Ricerca S.C.A.R.L/Rome/ROME//RoME//data//Maturity_parameters.rda")
   load("C:/Users/Bitetto Isabella/OneDrive - Coispa Tecnologia & Ricerca S.C.A.R.L/Rome/ROME//RoME//data//TM_list.rda")
-  check_species_TBTC(DataTargetSpecies,ResultTB,ResultTC,wd,suffix)
+  check_species_TBTC(ResultTB,ResultTC,DataTargetSpecies,wd,suffix)
   }
 
 
