51ee05796b09d4f71f6e8cd0d3cc2392153ddbd6
Walter <zupa@coispa.eu>
2020-03-02 14:23:53 +0100

    check

--- RoME.Rcheck/00_pkg_src/RoME/R/check_stratum_code.r
+++ RoME.Rcheck/00_pkg_src/RoME/R/check_stratum_code.r
@@ -1,47 +1,65 @@
 ###########################################################################################################################
-#   RoME: R code to perform multiple checks on MEDITS Survey data (TA, TB, TC and TE files - old and new MEDITS formats)  #                   
-#   Authors: I. Bitetto, M.T. Facchini, M.T. Spedicato                                                                    #
+#   RoME: R code to perform multiple checks on MEDITS Survey data (TA, TB, TC and TE files - old and new MEDITS formats)                                   #
+#   Authors: I. Bitetto, W. Zupa, M.T. Spedicato                                                                    #
 #   Coispa Tecnologia & Ricerca - Stazione sperimentale per lo Studio delle Risorse del Mare                              #
-#   If you have any comments or suggestions please contact the following e-mail address: bitetto@coispa.it                #                                                                                  
-#   March 2013                                                                                                            #
+#   If you have any comments or suggestions please contact the following e-mail address: bitetto@coispa.it                #
+#   March 2020
 ################################################################################
+
 # Check if the stratum code in TA (or TT) is consistent with the stratification code table
 
+if (FALSE){
+  ResultDataTA = read.csv("C:/Users/Bitetto Isabella/OneDrive - Coispa Tecnologia & Ricerca S.C.A.R.L/Rome/ROME/data/TA_GSA18_1994-2018.csv", sep=";")
+  ResultDataTA=ResultDataTA[ResultDataTA$YEAR==1994,]
+  #ResultTB = read.csv("C:/Users/Bitetto Isabella/OneDrive - Coispa Tecnologia & Ricerca S.C.A.R.L/Rome/ROME/data/TB_GSA18_1994-2018.csv", sep=";")
+
+  wd <- "C:/Users/Bitetto Isabella/OneDrive - Coispa Tecnologia & Ricerca S.C.A.R.L/Rome/ROME/temp"
+  suffix=paste(as.character(Sys.Date()),format(Sys.time(), "_time h%Hm%Ms%OS0"),sep="")
+  #load("C:/Users/Bitetto Isabella/OneDrive - Coispa Tecnologia & Ricerca S.C.A.R.L/Rome/ROME//RoME//data//DataTargetSpecies.rda")
+  #load("C:/Users/Bitetto Isabella/OneDrive - Coispa Tecnologia & Ricerca S.C.A.R.L/Rome/ROME//RoME//data//Maturity_parameters.rda")
+  #load("C:/Users/Bitetto Isabella/OneDrive - Coispa Tecnologia & Ricerca S.C.A.R.L/Rome/ROME//RoME//data//TM_list.rda")
+  check_stratum(ResultDataTA,wd,suffix)
+}
+
 check_stratum_code <- function (){
- 
-  
-  numberError = 0
-  
-  if (Format=="before_2012"){
-    Dataset = ResultDataTT #read.csv(paste(DataTT,".csv",sep=""), sep=";",header=TRUE)
-    ResultData = ResultDataTA # read.csv(paste(DataTA,".csv",sep=""), sep=";", header=TRUE)
-    ResultData = ResultData[ResultData$HAUL_NUMBER %in% Dataset$HAUL_NUMBER,]
-    ResultData= ResultData[order(ResultData$HAUL_NUMBER),]
-    Dataset= Dataset[order(Dataset$HAUL_NUMBER),]
-  } else {
-    Dataset = ResultDataTA #read.csv(paste(DataTA,".csv",sep=""), sep=";",header=TRUE)
+
+  Format="from_2012"
+  if (!file.exists(paste(wd,"Logfiles",sep="/"))){
+    dir.create(file.path(wd, "Logfiles"), showWarnings = FALSE)
   }
+
+  if (!exists("suffix")){
+    suffix=paste(as.character(Sys.Date()),format(Sys.time(), "_time h%Hm%Ms%OS0"),sep="")
+  }
+
+  Errors <- paste(wd,"/Logfiles/Logfile_",suffix,".dat",sep="")
+
+  numberError = 0
+
+
+    Dataset = ResultDataTA #read.csv(paste(DataTA,".csv",sep=""), sep=";",header=TRUE)
+
    write(paste("
               ----------- check correctness of stratum code - ",Dataset$YEAR[1]), file = Errors, append = TRUE)
-			  
+
   Strat_table = read.csv(paste(Stratification,".csv",sep=""), sep=";",header=TRUE)
-  
+
   Strat_table = Strat_table[Strat_table$GSA== Dataset$AREA[1],] # selection of the area
-  
+
   if (Format=="before_2012"){
     mean_depth = rowMeans(cbind(ResultData$SHOOTING_DEPTH,ResultData$HAULING_DEPTH))
-    
+
     for (i in 1:nrow(Dataset))
     {
       # check if the stratum code in TT is one of the codes in the Stratification table
-      
+
       if (!(Dataset$STRATUM[i] %in%  Strat_table$STRATUM)) {
         numberError = numberError+1
         write(paste("Warning: Haul ",Dataset$HAUL_NUMBER[i],"wrong stratum code"), file = Errors, append = TRUE)
-        
+
       }
-      
-      
+
+
       if ((mean_depth[i] >=10) & (mean_depth[i] <=50)) {
         Strat_table_temp = Strat_table[Strat_table$CODE==1,]
         if (!(Dataset$STRATUM[i] %in% Strat_table_temp$STRATUM)){
@@ -73,22 +91,22 @@ check_stratum_code <- function (){
           numberError ==numberError +1
         }
       }
-      
+
     }
   }else {
     mean_depth = rowMeans(cbind(Dataset$SHOOTING_DEPTH,Dataset$HAULING_DEPTH))
-  
+
   for (i in 1:nrow(Dataset))
   {
     # check if the stratum code in TA is one of the codes in the Stratification table
-    
+
   if (!(Dataset$NUMBER_OF_THE_STRATUM[i] %in%  Strat_table$STRATUM)) {
     numberError = numberError+1
     write(paste("Warning: Haul ",Dataset$HAUL_NUMBER[i],"wrong stratum code"), file = Errors, append = TRUE)
-   
+
     }
-  
-  
+
+
   if ((mean_depth[i] >=10) & (mean_depth[i] <=50)) {
     Strat_table_temp = Strat_table[Strat_table$CODE==1,]
     if (!(Dataset$NUMBER_OF_THE_STRATUM[i] %in% Strat_table_temp$STRATUM)){
@@ -120,16 +138,16 @@ check_stratum_code <- function (){
       numberError ==numberError +1
     }
   }
-  
+
   }
   }
-  
+
   if (numberError ==0) {
     write(paste("No error occurred"), file = Errors, append = TRUE)
-  }  
+  }
 
     return(TRUE)
-  
-  
-  
+
+
+
 }
