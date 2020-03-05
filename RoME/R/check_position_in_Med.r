95409f2594dbcec4f20215d60b13a4bef6f72cd0
Walter <zupa@coispa.eu>
2020-03-05 11:43:25 +0100

    check_position_MED

--- RoME/R/check_position_in_Med.r
+++ RoME/R/check_position_in_Med.r
@@ -7,13 +7,15 @@
 ############################################################################################################################
 # Check if the coordinates are in the Mediterranean Sea
 
-check_position_in_Med<-function(ResultDataTA){
+check_position_in_Med<-function(DataTA,wd,suffix){
 
   if (FALSE){
     library(RoME)
     wd <- tempdir()
     suffix=paste(as.character(Sys.Date()),format(Sys.time(), "_time h%Hm%Ms%OS0"),sep="")
-    DataTA = MEDITS::TA
+    DataTA = read.csv("~/GitHub/RoME/data/TA_GSA18_1994-2018.csv", sep=";") #     MEDITS::TA   #
+    DataTA[DataTA$YEAR ==2008 , ]
+
     # check_position_in_Med(DataTA, wd, suffix)
   }
 
@@ -28,40 +30,22 @@ check_position_in_Med<-function(ResultDataTA){
   }
   Errors <- paste(wd,"/Logfiles/Logfile_",suffix,".dat",sep="")
 
-  ResultData = ResultDataTA #read.csv(paste(DataTA,".csv",sep=""), sep=";", header=TRUE)
-  write(paste("
-              ----------- check if the hauls positions are in Mediterranean Sea - ",ResultData$YEAR[1]), file = Errors, append = TRUE)
+  ResultData = DataTA
+
+  write(paste("\n----------- check if the hauls positions are in Mediterranean Sea - ",ResultData$YEAR[1]), file = Errors, append = TRUE)
 
   ResultData=ResultData[ResultData$VALIDITY=="V",]
-  ResultData=convert_coordinates(ResultData)
-  for (i in 1:nrow(ResultData)){
-    if (as.character(ResultData$HAULING_QUADRANT[i])=="7"){
-      ResultData$lon_start[i]=(-1)* ResultData$lon_start[i]
-      ResultData$lon_end[i]=(-1)* ResultData$lon_end[i] }
-  }
 
+  k=1
   for (k in 1:nrow(ResultData)){
-    if (min(ResultData$lon_start[k])<(-5.4) | max(ResultData$lon_start[k])>35){
+    df <- ResultData[k,]
+    haul_on_land <- MEDITS::land.points(df, land=MEDITS::countries, verbose=FALSE)
+    if (length(haul_on_land)>1){
       numberError = numberError +1
-      write(paste("Haul ",ResultData$HAUL_NUMBER[k]," Error in start longitude", sep=""), file = Errors, append = TRUE)
+      write(paste("Haul ",haul_on_land$HAUL_NUMBER[1]," coordinates could likely fall on land", sep=""), file = Errors, append = TRUE)
     }
-  }
-  for (k in 1:nrow(ResultData)){
-    if (min(ResultData$lon_end[k])<(-5.4) | max(ResultData$lon_end[k])>35){
-      numberError = numberError +1
-      write(paste("Haul ",ResultData$HAUL_NUMBER[k]," Error in end longitude", sep=""), file = Errors, append = TRUE)
-    }    }
-  for (k in 1:nrow(ResultData)){
-    if (min(ResultData$lat_start[k])<(34) | max(ResultData$lat_start[k])>46){
-      numberError = numberError +1
-      write(paste("Haul ",ResultData$HAUL_NUMBER[k]," Error in start latitude", sep=""), file = Errors, append = TRUE)
     }
-  }
-  for (k in 1:nrow(ResultData)){
-    if (min(ResultData$lat_end[k])<(34) | max(ResultData$lat_end[k])>46){
-      numberError = numberError +1
-      write(paste("Haul ",ResultData$HAUL_NUMBER[k]," Error in end latitute", sep=""), file = Errors, append = TRUE)
-    }   }
+
   if (numberError ==0) {
     write(paste("No error occurred"), file = Errors, append = TRUE)
   }
@@ -71,4 +55,3 @@ check_position_in_Med<-function(ResultDataTA){
   } else { return(FALSE) }
 
 }
-################################################################################
