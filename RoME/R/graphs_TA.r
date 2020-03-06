907b1fbc3ce90ee591cbb98f4023f31ea5abe779
Walter <zupa@coispa.eu>
2020-03-06 09:24:06 +0100

    Update graphs_TA.r

--- RoME/R/graphs_TA.r
+++ RoME/R/graphs_TA.r
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-###########################################################################################################################
-#   RoME: R code to perform multiple checks on MEDITS Survey data (TA, TB, TC and TE files - old and new MEDITS formats)                                   #
-#   Authors: I. Bitetto, M.T. Facchini, M.T. Spedicato                                                                    #
-#   Coispa Tecnologia & Ricerca - Stazione sperimentale per lo Studio delle Risorse del Mare                              #
-#   If you have any comments or suggestions please contact the following e-mail address: bitetto@coispa.it                #
-#   March 2013                                                                                                            #
-###########################################################################################################################
-# Qualitative control (by means of 2 graphs) of relation between shooting depth e warp opening and between warp length e wing opening
-
-if (FALSE){
-  ResultDataTA = read.csv("C:/Users/Bitetto Isabella/OneDrive - Coispa Tecnologia & Ricerca S.C.A.R.L/Rome/ROME/data/TA_GSA18_1994-2018.csv", sep=";")
-  wd <- "C:/Users/Bitetto Isabella/OneDrive - Coispa Tecnologia & Ricerca S.C.A.R.L/Rome/ROME/temp"
-  #suffix=paste(as.character(Sys.Date()),format(Sys.time(), "_time h%Hm%Ms%OS0"),sep="")
-  graphs_TA(ResultDataTA,wd)
-  }
-
-
-graphs_TA<-function(ResultDataTA,wd){
-
-  if (!file.exists(paste(wd,"Graphs",sep="/"))){
-    dir.create(file.path(wd, "Graphs"), showWarnings = FALSE)
-  }
-
-
-
-  Errors <- paste(wd,"/Logfiles/Logfile_",suffix,".dat",sep="")
-
-
-
-
-  ResultData = ResultDataTA #read.csv(paste(DataTA,".csv",sep=""), sep=";", header=TRUE)
-  ResultData=ResultData[ResultData$VALIDITY=="V",]
-  tiff(file=paste(wd,"/Graphs/qualitative_control_TA_", ResultData$YEAR[1], "_AREA_",ResultData$AREA[1],".tif",sep=""), width=21, height=29.7, bg="white", units="cm", compression="none",res=200)
-=======
 ############################################################################################################################
 #   RoME: R code to perform multiple checks on MEDITS Survey data (TA, TB, TC and TE files - old and new MEDITS formats)   #
 #   Authors: I. Bitetto, W. Zupa, M.T. Spedicato                                                                           #
@@ -79,7 +44,6 @@ graphs_TA<-function(DataTA,wd,suffix){
   ResultData = DataTA #read.csv(paste(DataTA,".csv",sep=""), sep=";", header=TRUE)
   ResultData=ResultData[ResultData$VALIDITY=="V",]
   tiff(file=paste(wd,"/Graphs/qualitative_control_TA_", ResultData$YEAR[1], "_AREA_",ResultData$AREA[1],".tif",sep=""),width=8, height=12, bg="white", units="in", res=300, compression = 'lzw', pointsize = 1/300)
->>>>>>> 4c9ea4b9c339f66f106239da8677ac763001ae2f
   par(mfrow=c(2,1), mai=c(0.3,0.8,0.8,0.3), omi=c(0.8,0.8,1,0.8))
   X=ResultData$SHOOTING_DEPTH
   Y=ResultData$WARP_LENGTH
