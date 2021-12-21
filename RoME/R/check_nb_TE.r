939610b47a7b247ce98a37e435e33dbbbdd22d20
Walter <7878326+wzupa@users.noreply.github.com>
2021-12-21 16:05:39 +0100

    20211221

--- RoME/R/check_nb_TE.r
+++ RoME/R/check_nb_TE.r
@@ -36,12 +36,13 @@ check_nb_TE<- function (DataTE,wd,suffix){
 
   # check on NO_PER_SEX_MEASURED_IN_SUB_SAMPLE_FOR_OTOLITH
   TE_temp1 = TE[as.character(TE$OTOLITH_SAMPLED) == "Y",]
-  TE_temp1 = aggregate(TE_temp1$TYPE_OF_FILE, by= list(TE_temp1$HAUL_NUMBER,
-                   TE_temp1$GENUS,TE_temp1$SPECIES, TE_temp1$SEX ,
-                   TE_temp1$NO_PER_SEX_MEASURED_IN_SUB_SAMPLE_FOR_OTOLITH),FUN="length") #
-  colnames(TE_temp1)=c("HAUL_NUMBER","GENUS","SPECIES","SEX","NO_PER_SEX_MEASURED_IN_SUB_SAMPLE_FOR_OTOLITH","NB")
+
 
   if (nrow(TE_temp1)!=0){
+    TE_temp1 = aggregate(TE_temp1$TYPE_OF_FILE, by= list(TE_temp1$HAUL_NUMBER,
+                                                         TE_temp1$GENUS,TE_temp1$SPECIES, TE_temp1$SEX ,
+                                                         TE_temp1$NO_PER_SEX_MEASURED_IN_SUB_SAMPLE_FOR_OTOLITH),FUN="length") #
+    colnames(TE_temp1)=c("HAUL_NUMBER","GENUS","SPECIES","SEX","NO_PER_SEX_MEASURED_IN_SUB_SAMPLE_FOR_OTOLITH","NB")
     j=1
     for (j in 1:nrow(TE_temp1)){
       if(TE_temp1$NB[j]!=TE_temp1$NO_PER_SEX_MEASURED_IN_SUB_SAMPLE_FOR_OTOLITH[j]){
@@ -55,10 +56,11 @@ check_nb_TE<- function (DataTE,wd,suffix){
 
   # check on NO_PER_SEX_MEASURED_IN_SUB_SAMPLE_FOR_WEIGHT
   TE_temp2 = TE[as.character(TE$INDIVIDUAL_WEIGHT) != "ND",]
-  TE_temp2 = aggregate(TE_temp2$TYPE_OF_FILE, by= list(TE_temp2$HAUL_NUMBER,TE_temp2$GENUS,TE_temp2$SPECIES, TE_temp2$SEX,TE_temp2$NO_PER_SEX_MEASURED_IN_SUB_SAMPLE_FOR_WEIGHT),FUN="length")
-  colnames(TE_temp2)=c("HAUL_NUMBER","GENUS","SPECIES","SEX","NO_PER_SEX_MEASURED_IN_SUB_SAMPLE_FOR_WEIGHT","NB")
 
   if (nrow(TE_temp2)!=0){
+    TE_temp2 = aggregate(TE_temp2$TYPE_OF_FILE, by= list(TE_temp2$HAUL_NUMBER,TE_temp2$GENUS,TE_temp2$SPECIES, TE_temp2$SEX,TE_temp2$NO_PER_SEX_MEASURED_IN_SUB_SAMPLE_FOR_WEIGHT),FUN="length")
+    colnames(TE_temp2)=c("HAUL_NUMBER","GENUS","SPECIES","SEX","NO_PER_SEX_MEASURED_IN_SUB_SAMPLE_FOR_WEIGHT","NB")
+
     for (j in 1:nrow(TE_temp2)){
       if(TE_temp2$NB[j]!=TE_temp2$NO_PER_SEX_MEASURED_IN_SUB_SAMPLE_FOR_WEIGHT[j]){
         write(paste(TE_temp2$HAUL_NUMBER[j],TE_temp2$GENUS[j],TE_temp2$SPECIES[j], TE_temp2$SEX[j],"inconsistent value for the field NO_PER_SEX_MEASURED_IN_SUB_SAMPLE_FOR_WEIGHT"), file = Errors, append = TRUE)
@@ -68,9 +70,10 @@ check_nb_TE<- function (DataTE,wd,suffix){
   } else {
     write("No weight measured.", file = Errors, append = TRUE)
   }
+
+
   # check on NO_PER_SEX_MEASURED_IN_SUB_SAMPLE_FOR_AGEING
   TE_temp3 = TE[as.character(TE$OTOLITH_READ) == "Y",]
-
 
   if (nrow(TE_temp3)!=0){
     TE_temp3 = aggregate(TE_temp3$TYPE_OF_FILE, by= list(TE_temp3$HAUL_NUMBER,TE_temp3$GENUS,TE_temp3$SPECIES, TE_temp3$SEX,TE_temp3$NO_PER_SEX_MEASURED_IN_SUB_SAMPLE_FOR_AGEING),FUN="length")
